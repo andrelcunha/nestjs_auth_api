ea5aa2dcfe363f61be57769eaf7901841226082a
Andre Cunha <andreluiscunha81@gmail.com>
2024-03-05 13:40:03 -0300

    encrypting password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaClient, User } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -11,17 +12,18 @@ export class AuthService {
 
   async validateUser(email: string, password: string): Promise<User | null> {
     const user = await this.prisma.user.findUnique({ where: { email } });
-    if (user && user.password === password) {
+    if (user && bcrypt.compare(password, user.password)) {
       return user;
     }
     return null;
   }
 
   async signup(email: string, password: string): Promise<User> {
+    const hashedPassword = await bcrypt.hash(password, 10);
     return this.prisma.user.create({
       data: {
         email,
-        password,
+        password: hashedPassword,
       },
     });
   }
